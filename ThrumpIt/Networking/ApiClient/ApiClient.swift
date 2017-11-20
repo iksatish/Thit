5cc577e15fdf7ff0279fb29ce7217438581c5e21
Srikanth <gsteja22@gmail.com>
2017-11-20 07:14:10 -0800

    Initial Setup

---
+++ ThrumpIt/Networking/ApiClient/ApiClient.swift
@@ -0,0 +1,76 @@
+//
+//  ApiClient.swift
+//  MyGrocery
+//
+//  Created by Satish Kumar R Kancherla on 10/26/17.
+//  Copyright © 2017 Thrumpit. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+import Alamofire
+import SwiftyJSON
+
+class ApiClient: NSObject{
+    class func requestGETURL(_ strURL: String, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void) {
+        Alamofire.request(strURL).responseJSON { (responseObject) -> Void in
+            
+            if responseObject.result.isSuccess {
+                let resJson = JSON(responseObject.result.value!)
+                success(resJson)
+            }
+            if responseObject.result.isFailure {
+                let error : Error = responseObject.result.error!
+                failure(error)
+            }
+        }
+    }
+    
+    class func requestPOSTURL(_ strURL : String, params : [String : AnyObject]?, headers : [String : String]?, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void){
+        
+        Alamofire.request(strURL, method: .post, parameters: params, encoding: JSONEncoding.default, headers: headers).responseJSON { (responseObject) -> Void in
+            
+            print(responseObject)
+            
+            if responseObject.result.isSuccess {
+                let resJson = JSON(responseObject.result.value!)
+                success(resJson)
+            }
+            if responseObject.result.isFailure {
+                let error : Error = responseObject.result.error!
+                failure(error)
+            }
+        }
+    }
+    
+    class func requestDELURL(_ strURL: String, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void) {
+        Alamofire.request(strURL, method: .delete).responseJSON { (responseObject) -> Void in
+            
+            if responseObject.result.isSuccess {
+                let resJson = JSON(responseObject.result.value!)
+                success(resJson)
+            }
+            if responseObject.result.isFailure {
+                let error : Error = responseObject.result.error!
+                failure(error)
+            }
+        }
+    }
+    
+    class func requestPUTURL(_ strURL: String, success:@escaping (JSON) -> Void, failure:@escaping (Error) -> Void) {
+        Alamofire.request(strURL, method: .put).responseJSON { (responseObject) -> Void in
+            
+            if responseObject.result.isSuccess {
+                let resJson = JSON(responseObject.result.value!)
+                success(resJson)
+            }
+            if responseObject.result.isFailure {
+                let error : Error = responseObject.result.error!
+                failure(error)
+            }
+        }
+    }
+    
+    
+
+}
